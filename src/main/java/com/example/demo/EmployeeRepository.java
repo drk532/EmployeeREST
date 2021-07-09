baa7bd156b19b87af6f733c59074ef499199fc6e
Deepak Reddy Kolagatla <59638952+drk532@users.noreply.github.com>
2021-07-09 15:25:00 +0530

    Update EmployeeRepository.java

--- src/main/java/com/example/demo/EmployeeRepository.java
+++ src/main/java/com/example/demo/EmployeeRepository.java
@@ -3,5 +3,7 @@ package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EmployeeRepository extends CrudRepository<EmployeeRest, Integer> {
+  
+    List<Employee> findAllByOrderByempSalaryAsc();
 
 }
