cb91e45efd816cf66ccd2ef3967b23fe793691ab
Deepak Reddy Kolagatla <59638952+drk532@users.noreply.github.com>
2021-07-09 15:10:40 +0530

    Update EmployeeRestAppApplication.java

--- src/main/java/com/example/demo/EmployeeRestAppApplication.java
+++ src/main/java/com/example/demo/EmployeeRestAppApplication.java
@@ -2,12 +2,28 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 public class EmployeeRestAppApplication {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeRestAppApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		employeeRepository.save(new Employee("ABC","Devops",3434323));
+		employeeRepository.save(new Employee("XYZ","Security",4323234));
+		employeeRepository.save(new Employee("UVW","Programming",3232311));
+		employeeRepository.save(new Employee("GHI","Security","2211121"));
+		
+		public List<Employee> findAllOrderBySalaryAsc() {        
+        return employeeRepository.findAllOrderBySalaryAsc();
+    }
+		
 	}
 
 }
